a9a5bfe92ac35f04f4f5a23e8fd1d6dff2c6f2a3
xiaoyao <xiaoyao@yiji.com>
2018-04-13 17:15:10 +0800

    反射

--- src/com/xiaoyao/review/MainTest.java
+++ src/com/xiaoyao/review/MainTest.java
@@ -7,10 +7,29 @@ package com.xiaoyao.review;
  */
 public class MainTest {
     public static void main(String[] args) {
-
+        try {
+            MainTest.testReflect();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
     }
+
+    /**
+     * 类加载顺序
+     */
     public static void testClassSequ(){
         System.out.println("main start");
         Persion p = new SubPersion();
+    }
+
+    /**
+     * 反射
+     */
+    public static void testReflect() throws IllegalAccessException, InstantiationException {
+       Class cz = Persion.class;
+        System.out.println(cz.getName());
+        Persion o = (Persion)cz.newInstance();
     }
 }
